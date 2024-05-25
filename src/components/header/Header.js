29e9a6345d53b3b2594e4e7b8f8ce5b2552ae4cc
Thanh Nguyen <tnansci@gmail.com>
2024-05-24 20:35:51 -0400

    created Header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div>Gmail</div>
+      <div>Images</div>
+      <div>Search Labs</div>
+      <div>More</div>
+      <div>Account</div>
+    </div>
+  );
+}
+
+export default Header;
